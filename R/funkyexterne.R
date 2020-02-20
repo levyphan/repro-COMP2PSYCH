85c37781ea8168f355305f7fd6bfe0751aa8cd40
Rico Haas <rico.haas@hu-berlin.de>
2020-02-20 14:08:41 +0100

    why not

--- R/funkyexterne.R
+++ R/funkyexterne.R
@@ -3,6 +3,6 @@ if(!requireNamespace("pacman"))install.packages("pacman")
 pacman::p_load(here)
 
 source(here("R", "read.R"))
-
+#spiel und spaß
 ggplot(results, aes(diff, time, group = diff)) + geom_boxplot(color ="darkgreen") + ggtitle("Funky plot") + geom_violin()
 
