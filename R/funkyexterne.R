80bd7b440383cc9f38fe64c3d983fd364c04fef2
Le Vy Phan <levy.pha@gmail.com>
2020-02-20 13:53:22 +0100

    funky commit

---
+++ R/funkyexterne.R
@@ -0,0 +1,6 @@
+library(ggplot2)
+if(!requireNamespace("pacman"))install.packages("pacman")
+pacman::p_load(here)
+
+source(here("R", "read.R"))
+ggplot(results, aes(diff, time, group = diff)) + geom_boxplot()
