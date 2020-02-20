18726407b24a46fb582e93fd2a61ba2690a9fc22
Le Vy Phan <levy.pha@gmail.com>
2020-02-20 14:07:10 +0100

    zeilenspezifisch?

--- R/funkyexterne.R
+++ R/funkyexterne.R
@@ -3,5 +3,6 @@ if(!requireNamespace("pacman"))install.packages("pacman")
 pacman::p_load(here)
 
 source(here("R", "read.R"))
+
 ggplot(results, aes(diff, time, group = diff)) + geom_boxplot(color ="darkgreen") + ggtitle("Funky plot") + geom_violin()
 
